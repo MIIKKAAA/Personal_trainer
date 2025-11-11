3a7d505c1fca851be15a161366da0d877a514857
miikka.vartiainen@gmail.com <miikka.vartiainen@gmail.com>
2025-11-11 23:32:43 +0200

    Modified README

---
+++ src/components/CustomerTable.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import {useState, useEffect} from "react";
+
+
+function CustomerTable() {
+    const [customers, setCustomers] = useState([]);
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default CustomerTable;
---
+++ src/components/TrainingTable.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import {useState, useEffect} from "react";
+
+
+function TrainingTable() {
+    const [trainings, setTrainings] = useState([]);
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default TrainingTable;
